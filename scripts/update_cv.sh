10934bed2913bef56e2fe37afdfdcf3a4c7f1767
MMuthukrishna <muthukrishna.m@instamojo.com>
2021-02-08 17:56:24 +0530

    Update update_cv script

--- scripts/update_cv.sh
+++ scripts/update_cv.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-pdflatex -output-directory content/cv ./content/cv/cv.tex
+CV_PATH='content/cv'
+pdflatex -output-directory $CV_PATH ./$CV_PATH/cv.tex
+pdfjam $CV_PATH/cv.pdf 1 -o $CV_PATH/cv-1.pdf
+pdfjam $CV_PATH/cv.pdf 2 -o $CV_PATH/cv-2.pdf
+pdfjam $CV_PATH/cv-1.pdf $CV_PATH/cv-2.pdf --nup 2x1 --landscape --outfile $CV_PATH/cv-landscape.pdf
+rm $CV_PATH/cv-1.pdf
+rm $CV_PATH/cv-2.pdf
 git add content/cv/cv.tex
 git add content/cv/cv.pdf
+git add content/cv/cv-landscape.pdf
 git commit -m "Update cv"
 git push
 ./scripts/deploy.sh
